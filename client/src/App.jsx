efc3986e954fc3edc593c6fdebb1e96e274a4b78
Netpoc <ayenotopeter@hotmail.com>
2023-11-06 08:37:39 +0100

    moved rep from client

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/Signin";
+import Header from "./components/Header";
+export default function App() {
+  return <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>;
+}
